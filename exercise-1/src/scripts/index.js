3258f8eef83890460ae1f4273d4eaa3e81e300c7
OMPRAKASH SAH KANU <omprakashsahkanu@OMPRAKASHs-MacBook-Pro.local>
2022-02-10 14:03:14 -0700

    requirejs and handlebars included

---
+++ exercise-1/src/scripts/index.js
@@ -0,0 +1,10 @@
+requirejs(['external/handlebars'], function(HandleBars){
+    const users = fetch("https://615485ee2473940017efaed3.mockapi.io/assessment")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+  
+  var template = HandleBars.compile("Handlebars <b>{{doesWhat}}</b>");
+
+  console.log(template({ doesWhat: "rocks!" }));
+  
+})
