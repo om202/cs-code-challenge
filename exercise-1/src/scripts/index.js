214da47e7a48f430928432dfcd7b3f151dc29733
OMPRAKASH SAH KANU <omprakashsahkanu@OMPRAKASHs-MacBook-Pro.local>
2022-02-10 17:17:15 -0700

    issue with button

--- exercise-1/src/scripts/index.js
+++ exercise-1/src/scripts/index.js
@@ -1,10 +1,32 @@
-requirejs(['external/handlebars'], function(HandleBars){
-    const users = fetch("https://615485ee2473940017efaed3.mockapi.io/assessment")
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-  
-  var template = HandleBars.compile("Handlebars <b>{{doesWhat}}</b>");
+requirejs(["external/handlebars"], function (HandleBars) {
 
-  console.log(template({ doesWhat: "rocks" }));
-  
-})
+  function showDetails(id){
+    let ulElement = document.getElementById(id);
+    let createdAt = ulElement.getElementsByClassName('user-created-at')
+    let userId = ulElement.getElementsByClassName('user-id')
+  }
+
+  function generateHTML(users) {
+
+    let rawTemplate = document.getElementById("exercise-1").innerHTML;
+
+    let template = HandleBars.compile(rawTemplate);
+    let generatedHTML = template({
+      avatar: users.avatar,
+      name: users.name,
+      createdAt: new Date(users.createdAt).toDateString(),
+      id: users.id,
+    });
+
+    let mainContainer = document.getElementById("main");
+    mainContainer.innerHTML += generatedHTML;
+  }
+
+  const users = fetch("https://615485ee2473940017efaed3.mockapi.io/assessment")
+    .then((res) => res.json())
+    .then((users) => {
+      for (let i = 0; i < users.length; i++) {
+        generateHTML(users[i]);
+      }
+    });
+});
